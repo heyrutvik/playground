4a94ef5ca367fbf3385cdd4b227dda4528835363
RR <heyrutvik@gmail.com>
2018-05-17 10:01:56 +0530

    church numerals

--- js/fizz-buzz.js
+++ js/fizz-buzz.js
@@ -1,15 +1,10 @@
 // write fizz-buzz program using most fundamental thing.. functions!
 
-var debug = true;
-separator = function() {
-  console.log("\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-}
-
 const range = (start, end) => (
   Array.from(Array(end - start + 1).keys()).map(i => i + start)
 );
 
-var fizzBuzzString = range(1, 100).map(n => {
+let fizzBuzzString = range(1, 100).map(n => {
   if (n % 15 === 0) {
     return 'FizzBuzz'
   } else if (n % 3 === 0) {
@@ -20,11 +15,7 @@ var fizzBuzzString = range(1, 100).map(n => {
     return n
   }
 })
-
-if (debug) {
-  console.log(fizzBuzzString.join(', '))
-  separator()
-}
+//console.log(fizzBuzzString.join(', '))
 
 /**
 properties of a function:
@@ -34,27 +25,36 @@ properties of a function:
 */
 
 // number representation as function - aka Church Numerals
-
-ZERO = function(f) {
+let ZERO = function(f) {
   return function(x) {
     return x
   }
 }
-
-ONE = function(f) {
+let ONE = function(f) {
   return function(x) {
     return f(x)
   }
 }
+let TWO = function(f) {
+  return function(x) {
+    return f(f(x))
+  }
+}
+let HUNDRED = function(f) {
+  return function(x) {
+    return f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(f(x))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
+  }
+}
 
 // method to convert church numerals(cn) to js number
-toInt = function(cn) {
+let toInt = function(cn) {
   return cn(function(n) {
     return n + 1
   })(0);
 }
-if (debug) {
-  console.log("cn zero as js number: " +toInt(ZERO))
-  console.log("cn one as js number: " +toInt(ONE))
-  separator()
-}
+/*
+console.log("church numeral zero as js number: " +toInt(ZERO))
+console.log("church numeral one as js number: " +toInt(ONE))
+console.log("church numeral two as js number: " +toInt(TWO))
+console.log("church numeral hundred as js number: " +toInt(HUNDRED))
+*/
