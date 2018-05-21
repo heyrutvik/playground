2240766b22123ed6d2e7e5da4bc586d1876fbd79
Rutvik <heyrutvik@github.com>
2018-05-21 21:05:30 +0530

    church boolean, predicate, pair, numeric opeartions

--- js/fizz-buzz.js
+++ js/fizz-buzz.js
@@ -58,3 +58,144 @@ console.log("church numeral one as js number: " +toInt(ONE))
 console.log("church numeral two as js number: " +toInt(TWO))
 console.log("church numeral hundred as js number: " +toInt(HUNDRED))
 */
+
+// church boolean
+let TRUE = function(x) {
+  return function(y) {
+    return x
+  }
+}
+
+let FALSE = function(x) {
+  return function(y) {
+    return y
+  }
+}
+
+// method to convert church boolean(cb) to js boolean
+let toBoolean = function(cb) {
+  return cb(true)(false)
+}
+/*
+console.log(toBoolean(TRUE))
+console.log(toBoolean(FALSE))
+*/
+
+// church if condition
+let IF = function(cb) {
+  return function(c) {
+    return function(a) {
+      return cb(c)(a)
+    }
+  }
+}
+/*
+console.log(IF(TRUE)("happy")("sad"))
+console.log(IF(FALSE)("happy")("sad"))
+*/
+
+// === 0 predicate
+let IS_ZERO = function(cn) {
+  return cn(function(x) { return FALSE })(TRUE)
+}
+/*
+console.log(toBoolean(IS_ZERO(ONE)))
+console.log(toBoolean(IS_ZERO(ZERO)))
+*/
+
+
+// pair data structure
+let PAIR = function(x) {
+  return function(y) {
+    return function(f) {
+      return f(x)(y)
+    }
+  }
+}
+let LEFT = function(p) {
+  return p(function(x) {
+    return function(y) {
+      return x
+    }
+  })
+}
+let RIGHT = function(p) {
+  return p(function(x) {
+    return function(y) {
+      return y
+    }
+  })
+}
+/*
+let pair1 = PAIR(1)(2)
+console.log(LEFT(pair1))
+console.log(RIGHT(pair1))
+*/
+
+// increment operation on church numeral
+let INCREMENT = function(cn) {
+  return function(f) {
+    return function(x) {
+      return f(cn(f)(x))
+    }
+  }
+}
+/*
+console.log(toInt(INCREMENT(HUNDRED)))
+*/
+
+/* decrement using pair
+ * (zero, zero) as input
+ * slide as function
+ * after appling n times, take left
+ */
+let SLIDE = function(p) {
+  return PAIR(RIGHT(p))(INCREMENT(RIGHT(p)))
+}
+let DECREMENT = function(cn) {
+  return LEFT(cn(SLIDE)(PAIR(ZERO)(ZERO)))
+}
+/*
+console.log(toInt(DECREMENT(ONE)))
+console.log(toInt(DECREMENT(HUNDRED)))
+*/
+
+// addition
+let ADD = function(cn1) {
+  return function(cn2) {
+    return cn2(INCREMENT)(cn1)
+  }
+}
+/*
+console.log(toInt(ADD(ONE)(ONE)))
+*/
+
+// subtract
+let SUBTRACT = function(cn1) {
+  return function(cn2) {
+    return cn2(DECREMENT)(cn1)
+  }
+}
+/*
+console.log(toInt(SUBTRACT(TWO)(TWO)))
+*/
+
+// multiply
+let MULTIPLY = function(cn1) {
+  return function(cn2) {
+    return cn2(ADD(cn1))(ZERO)
+  }
+}
+/*
+console.log(toInt(MULTIPLY(TWO)(TWO)))
+*/
+
+// power
+let POWER = function(cn1) {
+  return function(cn2) {
+    return cn2(MULTIPLY(cn1))(ONE)
+  }
+}
+/*
+console.log(toInt(POWER(TWO)(TWO)))
+*/
