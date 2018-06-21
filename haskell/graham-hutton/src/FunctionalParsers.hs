0c79b8f264d6adcf54719fc63d588396c2ba77a2
Rutvik <heyrutvik@gmail.com>
2018-06-21 09:08:33 +0530

    expr parser

--- haskell/graham-hutton/src/FunctionalParsers.hs
+++ haskell/graham-hutton/src/FunctionalParsers.hs
@@ -1,6 +1,7 @@
 module FunctionalParsers where
 
 import Prelude hiding ((>>=), return)
+import Data.Char
 
 type Parser a = String -> [(a, String)]
 
@@ -27,38 +28,114 @@ p >>= f = \x -> case parse p x of
 
 
 -- "abcde" -> [('a', 'c'), "de"]
-p' :: Parser (Char, Char)
-p' = item >>= (\x -> item >>= (\y -> item >>= (\z -> return (x, z))))
+p :: Parser (Char, Char)
+p = item >>= (\x -> item >>= (\y -> item >>= (\z -> return (x, z))))
 
 -- "abcde" -> [('a', 'c'), "de"]
-p'' :: Parser (Char, Char)
-p'' = item >>= \x ->
+p' :: Parser (Char, Char)
+p' = item >>= \x ->
       item >>= \y ->
       item >>= \z ->
       return (x, z)
 
 {-
-src/FunctionalParsers.hs:43:8: error:
-    • Couldn't match type ‘[(Char, String)]’ with ‘Char’
-      Expected type: String -> [((Char, Char), String)]
-        Actual type: Parser ([(Char, String)], [(Char, String)])
-    • In a stmt of a 'do' block: return (x, z)
-      In the expression:
-        do x <- item
-           y <- item
-           z <- item
-           return (x, z)
-      In an equation for ‘p’:
-          p = do x <- item
-                 y <- item
-                 z <- item
-                 ....
-   |
-43 |        return (x, z)
-   |        ^^^^^^^^^^^^^
--}
-p :: Parser (Char, Char)
-p = do x <- item
+p'' :: Parser (Char, Char)
+p'' = do x <- item
        y <- item
        z <- item
        return (x, z)
+-}
+
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = \x -> case parse p x of
+                  [] -> parse q x
+                  [(v, out)] -> [(v, out)]
+
+-- could be done with do notation
+sat :: (Char -> Bool) -> Parser Char
+sat p = item >>= \x -> if p x then return x else failure
+
+digit :: Parser Char
+digit = sat isDigit
+
+lower :: Parser Char
+lower = sat isLower
+
+upper :: Parser Char
+upper = sat isUpper
+
+letter :: Parser Char
+letter = sat isAlpha
+
+alphanum :: Parser Char
+alphanum = sat isAlphaNum
+
+char :: Char -> Parser Char
+char x = sat (==x)
+
+string :: String -> Parser String
+string [] = return []
+string (x : xs) = char x >>= (\_ -> string xs >>= (\_ -> return (x : xs)))
+
+many :: Parser a -> Parser [a]
+many p  = many1 p +++ return []
+
+many1 :: Parser a -> Parser [a]
+many1 p = p >>= (\v -> many p >>= (\vs -> return (v : vs)))
+
+ident :: Parser String
+ident = lower >>= (\x -> many alphanum >>= (\xs -> return (x : xs)))
+
+nat :: Parser Int
+nat = many1 digit >>= (\xs -> return (read xs))
+
+space :: Parser ()
+space = many (sat isSpace) >>= (\_ -> return ())
+
+token :: Parser a -> Parser a
+token p = space >>= (\_ -> p >>= (\v -> space >>= (\_ -> return v)))
+
+identifier :: Parser String
+identifier = token ident
+
+natural :: Parser Int
+natural = token nat
+
+symbol :: String -> Parser String
+symbol xs = token (string xs)
+
+p''' :: Parser [Int]
+p''' = symbol "[" >>= \_ ->
+       natural >>= \n ->
+       many (symbol "," >>= \_ -> natural) >>= \ns ->
+       symbol "]" >>= \_ ->
+       return (n : ns)
+
+expr :: Parser Int
+expr = term >>= (\t -> (symbol "+" >>= \_ -> expr >>= \e -> return (t + e)) +++ return t)
+
+term :: Parser Int
+term = factor >>= (\f -> (symbol "*" >>= \_ -> term >>= \t -> return (f * t)) +++ return f)
+
+factor :: Parser Int
+factor = (symbol "(" >>= \_ ->
+         expr >>= \e ->
+         symbol ")" >>= \_ ->
+         return e) +++ natural
+
+eval :: String -> Int
+eval xs = case parse expr xs of
+            [(n, [])] -> n
+            [(_, out)] -> error ("unconsumed input " ++ out)
+            [] -> error "invalid input"
+
+-- Exercises
+
+int :: Parser Int
+int = (symbol "-" >>= \_ -> natural >>= \n -> return (- n)) +++ natural
+
+newline :: Parser Char
+newline = char '\n'
+
+comment :: Parser ()
+comment = symbol "--" >>= \_ -> many alphanum >>= \_ -> (newline >>= \_ -> return ()) +++ failure
