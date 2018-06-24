1dd5b488ae7752a4ea667c6a84893b57135dac46
Rutvik <heyrutvik@gmail.com>
2018-06-24 15:06:11 +0530

    tautology checker

--- haskell/graham-hutton/src/DeclaringTypes.hs
+++ haskell/graham-hutton/src/DeclaringTypes.hs
@@ -1,6 +1,8 @@
 module DeclaringTypes where
 
 -- type declarations
+type Bit = Int
+
 type String = [Char]
 
 type Board = [Pos]
@@ -84,3 +86,76 @@ occurs m (Node l n r) = m == n || occurs m l || occurs m r
 flatten :: Tree -> [Int]
 flatten (Leaf n)     = [n]
 flatten (Node l n r) = flatten l ++ [n] ++ flatten r
+
+-- tautology checker
+data Prop
+  = Const Bool
+  | Var Char
+  | Not Prop
+  | And Prop
+        Prop
+  | Imply Prop
+          Prop
+
+-- A /\ ~A
+p1 :: Prop
+p1 = And (Var 'A') (Not (Var 'A'))
+
+-- (A /\ B) => A
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+-- A => (A /\ B)
+p3 :: Prop
+p3 = Imply (Var 'A') (And (Var 'A') (Var 'B'))
+
+-- (A /\ (A => B)) => B
+p4 :: Prop
+p4 = Imply (And (Var 'A') (Imply (Var 'A') (Var 'B'))) (Var 'B')
+
+type Subst = Assoc Char Bool
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b)   = b
+eval s (Var x)     = find x s
+eval s (Not p)     = not (eval s p)
+eval s (And p q)   = eval s p && eval s q
+eval s (Imply p q) = eval s p <= eval s q
+
+vars :: Prop -> [Char]
+vars (Const _)   = []
+vars (Var x)     = [x]
+vars (Not p)     = vars p
+vars (And p q)   = vars p ++ vars q
+vars (Imply p q) = vars p ++ vars q
+
+bools :: Int -> [[Bool]]
+bools n = map f [0 .. limit]
+  where
+    limit = (2 ^ n) - 1 -- max list we need
+    make n bs = take n (bs ++ repeat 0) -- append 0 till we get n bit-length
+    conv 0 = False -- convert 0 to False
+    conv 1 = True -- convert 1 to True
+    f = map conv . make n . int2bin -- integer to binary then adject the length then convert each 0/1 to boolean
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+bools' :: Int -> [[Bool]]
+bools' 0 = [[]]
+bools' n = map (False :) bss ++ map (True :) bss
+  where
+    bss = bools' (n - 1)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups []     = []
+rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+
+substs :: Prop -> [Subst]
+substs p = map (zip vs) (bools (length vs))
+  where
+    vs = rmdups (vars p)
+
+isTaut :: Prop -> Bool
+isTaut p = and [eval s p | s <- substs p]
