6fa253f798234ee2b5d82b864a1981a81f91b401
Rutvik Patel <heyrutvik@gmail.com>
2019-03-06 01:45:07 +0530

    minor changes

--- scala/turing/src/main/scala/machine/standard/Demo.scala
+++ scala/turing/src/main/scala/machine/standard/Demo.scala
@@ -12,11 +12,9 @@ object Demo extends App {
   val t1 = machine.regular.Demo.t1
   val t2 = machine.regular.Demo.t2
 
-  println(t1.prettyPrint)
   val t3 = standardForm(t1.mkDSL)._3.toStandardTable
-  List(t3.toStandardForm, t3.toStandardDescription, t3.toDescriptionNumber) foreach println
+  List(t1.prettyPrint, t3.toStandardForm, t3.toStandardDescription, t3.toDescriptionNumber) foreach println
 
-  println(t1.prettyPrint)
   val t4 = standardForm(t2.mkDSL)._3.toStandardTable
-  List(t4.toStandardForm, t4.toStandardDescription, t4.toDescriptionNumber) foreach println
+  List(t2.prettyPrint, t4.toStandardForm, t4.toStandardDescription, t4.toDescriptionNumber) foreach println
 }
